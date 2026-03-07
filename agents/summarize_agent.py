fafb98c29b57f4a0c2636012d5339c9b40fbac90
Ayushii-Sharma20 <138204247+Ayushii-Sharma20@users.noreply.github.com>
2026-03-08 04:01:26 +0530

    FAcebook data added(300mb)

--- agents/summarize_agent.py
+++ agents/summarize_agent.py
@@ -2,16 +2,9 @@ from transformers import pipeline
 
 summarizer = pipeline(
     "summarization",
-    model="facebook/bart-large-cnn"
+    model="sshleifer/distilbart-cnn-12-6"
 )
 
 def summarize_text(text):
-
-    result = summarizer(
-        text,
-        max_length=120,
-        min_length=30,
-        do_sample=False
-    )
-
+    result = summarizer(text, max_length=120, min_length=30, do_sample=False)
     return result[0]["summary_text"]
